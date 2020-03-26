753a93107eb3b96ccb81f83792533fdfd012d403
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-03-26 20:25:25 +0900

    Update MissingIntegers.py

--- src/Codility/MissingIntegers.py
+++ src/Codility/MissingIntegers.py
@@ -2,11 +2,14 @@
 https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/
 Using Hashmap
 """
-dic = {}
-for num in A:
-    dic[num] = 0
-i = 1
-while True:
-    if i not in dic.keys():
-        print(i)
-        break
+def solution(A):
+    # write your code in Python 3.6
+    dic = {}
+    for num in A:
+        dic[num] = 0
+    i = 1
+    while True:
+        if i not in dic.keys():
+            return i
+            break
+        i += 1
