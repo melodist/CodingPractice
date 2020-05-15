61bb4767ac3b77906b2903d658e8257306c03bf2
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-05-15 13:24:32 +0900

    Update Sort_2.py

--- src/Baekjoon/Sort_2.py
+++ src/Baekjoon/Sort_2.py
@@ -1,6 +1,7 @@
 """
 https://www.acmicpc.net/problem/2751
 1. Merge Sort
+2. Heap Sort
 """
 import sys
 
@@ -37,3 +38,35 @@ def merge(array, begin, end):
 
 merge(a, 0, n-1)
 [print(num) for num in a]
+
+
+# 2. Heap Sort
+v = [int(sys.stdin.readline().rstrip()) for _ in range(int(input())]
+ 
+def heapify(li, idx, n):
+    l = idx * 2;
+    r = idx * 2 + 1
+    s_idx = idx
+    if (l <= n and li[s_idx] > li[l]):
+        s_idx = l
+    if (r <= n and li[s_idx] > li[r]):
+        s_idx = r
+    if s_idx != idx:
+        li[idx], li[s_idx] = li[s_idx], li[idx]
+        return heapify(li, s_idx, n)
+ 
+def heap_sort(v) :
+    n = len(v)
+    v = [0]+v
+ 
+    # create min heap
+    for i in range(n, 0, -1) :
+        heapify(v, i, n)
+ 
+    # min element extract & heap
+    for i in range(n, 0, -1) :
+        print(v[1])
+        v[i], v[1] = v[1], v[i]
+        heapify(v, 1, i-1)
+ 
+heap_sort(v)
