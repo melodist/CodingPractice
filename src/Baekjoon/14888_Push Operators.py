17a4dfee9514a0935050d4c24ba13c24773ad358
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-05-27 00:10:50 +0900

    Update 14888_Push Operators.py

--- src/Baekjoon/14888_Push Operators.py
+++ src/Baekjoon/14888_Push Operators.py
@@ -2,6 +2,7 @@
 https://www.acmicpc.net/problem/14888
 Use array for visited permutation
 """
+#1. Brute Force
 from itertools import permutations
 
 n = int(input())
@@ -37,4 +38,41 @@ for p in permutations(ops):
         
     visited.add(p)
     
+print(ans_max, ans_min, sep='\n')
+
+
+#2. BackTracking
+n = int(input())
+nums = list(map(int, input().split()))
+operators = list(map(int, input().split()))
+
+def backtrack(value, l, plus, minus, times, div):
+    global ans_min, ans_max
+    if l == n - 1:
+        if ans_min > value:
+            ans_min = value
+        if ans_max < value:
+            ans_max = value
+        return
+    
+    if plus:
+        backtrack(value + nums[l+1], l+1, plus-1, minus, times, div)
+        
+    if minus:
+        backtrack(value - nums[l+1], l+1, plus, minus-1, times, div)
+        
+    if times:
+        backtrack(value * nums[l+1], l+1, plus, minus, times-1, div)
+        
+    if div:
+        if value < 0:
+            backtrack(- ((-value) // nums[l+1]), l+1, plus, minus, times, div-1)
+        else:
+            backtrack(value // nums[l+1], l+1, plus, minus, times, div-1)
+            
+ans_min = 10E10
+ans_max = -10E10
+
+backtrack(nums[0], 0, *operators)
+
 print(ans_max, ans_min, sep='\n')
