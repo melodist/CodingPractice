b1df834055344bf03cdc7467ab2b7fcfc84cef4a
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-07-01 22:37:10 +0900

    Create Move Zeros.py

---
+++ src/LeetCode/Move Zeros.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+"""
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        p = 0
+        n = len(nums)
+        
+        for i in range(n):
+            if nums[i] != 0:
+                nums[p], nums[i] = nums[i], nums[p]
+                p += 1
