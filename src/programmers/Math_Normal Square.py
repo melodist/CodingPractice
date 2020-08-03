4e72961238d9fedefa8dd4c2f14cd1fbd2430435
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-08-03 23:58:57 +0900

    Update and rename Normal Square.py to Math_Normal Square.py

--- src/programmers/Math_Normal Square.py
+++ src/programmers/Math_Normal Square.py
@@ -1,6 +1,9 @@
 """
 https://programmers.co.kr/learn/courses/30/lessons/62048
 Using GCD
+If w and h are coprime, Diagonal passes (w-1) lines and (h-1) lines.
+So total broken square will be 1 + (w-1) + (h-1) = w + h - 1.
+If w and h has GCD, it will be w + h - gcd(w, h)
 """
 def gcd(a, b):
     return b if a % b == 0 else gcd(b, a % b)
