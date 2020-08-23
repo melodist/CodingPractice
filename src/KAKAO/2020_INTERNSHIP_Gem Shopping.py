417cc2c32959d28e04822a214925a41a89e7eb2e
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-08-24 00:28:09 +0900

    Update 2020_INTERNSHIP_Gem Shopping.py
    Add Other Solution

--- src/KAKAO/2020_INTERNSHIP_Gem Shopping.py
+++ src/KAKAO/2020_INTERNSHIP_Gem Shopping.py
@@ -2,6 +2,7 @@
 https://programmers.co.kr/learn/courses/30/lessons/67258
 Using Two Pointer Approach
 """
+#1. My Solution
 from collections import defaultdict
 
 
@@ -10,24 +11,58 @@ def solution(gems):
     two = set(gems)
 
     answer = [1, len(gems)]
-    print(answer[1] - answer[0])
     start = 0
     end = 0
-    while end < len(gems):
-        print(one)
+    
+    while True:
         if len(one) == len(two):
-            if end - start < answer[1] - answer[0]:
-                print(start+1, end+1)
-                answer = [start+1, end+1]
+            if end - start < answer[1] - answer[0] + 1:
+                answer = [start+1, end]
                 
             if one[gems[start]] == 1:
                 del one[gems[start]]
             else:
                 one[gems[start]] -= 1
+                
+            if start == len(gems):
+                break
             start += 1
         else:
+            if end == len(gems):
+                break
             one[gems[end]] += 1
             end += 1
             
+    return answer
+
+#2. Other Solution
+from collections import defaultdict
+
+
+def solution(gems):
+    candidates = []
+    start, end = 0, 0
+    gems_len, gem_kind = len(gems), len(set(gems))
+    answer = [1, gems_len]
+    gems_dict = defaultdict(int)
+    
+    while True:
+        kind = len(gems_dict)
+        if start == gems_len:
+            break
+        if kind == gem_kind:
+            if end - start < answer[1] - answer[0] + 1:
+                answer = [start+1, end]
+            gems_dict[gems[start]] -= 1
+            if gems_dict[gems[start]] == 0:
+                del gems_dict[gems[start]]
+            start += 1
+            continue
+        if end == gems_len:
+            break
+        if kind != gem_kind:
+            gems_dict[gems[end]] += 1
+            end += 1
+            continue
 
     return answer
