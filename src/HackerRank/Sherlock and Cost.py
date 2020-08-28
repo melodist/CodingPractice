c69ebc590dfcf54c1acfe9f53e5675d6b1693fca
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-08-28 11:33:25 +0900

    Update Sherlock and Cost.py

--- src/HackerRank/Sherlock and Cost.py
+++ src/HackerRank/Sherlock and Cost.py
@@ -5,6 +5,19 @@ Note that there are only 2 cases for choosing A[i] value.
 1. Choosing 1
 2. Choosing B[i]
 """
+#1. My Solution - Timeout in python 3. Accpeted in Pypy 3
+def cost(B):
+    n = len(B)
+    dp = [[0] * 101 for _ in range(n)]
+
+    for i in range(1, n):
+        for j in (1, B[i-1]):
+            dp[i][1] = max(dp[i][1], dp[i-1][j] + abs(j - 1))
+            dp[i][B[i]] = max(dp[i][B[i]], dp[i-1][j] + abs(j - B[i]))
+
+    return max(dp[-1])
+
+#2. Optimal Solution
 def cost(B):
     n = len(B)
     hi, low= 0, 0
