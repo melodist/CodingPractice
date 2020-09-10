241f93f92054db2068e69a8a8107692c98e06b08
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-09-10 16:13:07 +0900

    Update 1629_Multiply.py

--- src/Baekjoon/1629_Multiply.py
+++ src/Baekjoon/1629_Multiply.py
@@ -3,6 +3,7 @@ https://www.acmicpc.net/problem/1629
 Using divide and conquer
 MOD(a^2 % b) = MOD(a % b) ** 2
 """
+#1. My Solution
 def divide(a, b, c):
     d, e = divmod(b, 2)
     if b == 0:
@@ -15,3 +16,8 @@ def divide(a, b, c):
 
 a, b, c = map(int, input().split())
 print(divide(a, b, c))
+
+#2. Other Solution
+# Return x to the power y; if z is present, return x to the power y, modulo z 
+# (computed more efficiently than pow(x, y) % z)
+print(pow(*map(int,input().split())))
