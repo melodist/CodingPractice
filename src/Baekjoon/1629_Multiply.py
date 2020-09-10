3315bb60d36ec37e4f630689caff980b7221d001
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-09-10 16:10:54 +0900

    Create 1629_Multiply.py

---
+++ src/Baekjoon/1629_Multiply.py
@@ -0,0 +1,17 @@
+"""
+https://www.acmicpc.net/problem/1629
+Using divide and conquer
+MOD(a^2 % b) = MOD(a % b) ** 2
+"""
+def divide(a, b, c):
+    d, e = divmod(b, 2)
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a % c  # Note the case if b == 1
+        
+    return (((divide(a, d, c)) ** 2) * divide(a, e, c)) % c
+
+
+a, b, c = map(int, input().split())
+print(divide(a, b, c))
