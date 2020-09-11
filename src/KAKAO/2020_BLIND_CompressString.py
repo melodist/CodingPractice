b25ee173577e777fa0db9efc14a6e9dfe265524a
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-09-11 18:11:04 +0900

    Update 2020_BLIND_CompressString.py

--- src/KAKAO/2020_BLIND_CompressString.py
+++ src/KAKAO/2020_BLIND_CompressString.py
@@ -1,3 +1,8 @@
+"""
+https://programmers.co.kr/learn/courses/30/lessons/60057/
+Implementation Problem
+"""
+#1. My Solution
 import math
 
 def solution(s):
@@ -29,3 +34,21 @@ def test(s,i):
           return key + test(s_temp, i)
         else:
           return str(num) + key + test(s_temp, i)
+
+#2. Other Solution
+def compress(text, tok_len):
+    words = [text[i:i+tok_len] for i in range(0, len(text), tok_len)]
+    res = []
+    cur_word = words[0]
+    cur_cnt = 1
+    for a, b in zip(words, words[1:] + ['']):
+        if a == b:
+            cur_cnt += 1
+        else:
+            res.append([cur_word, cur_cnt])
+            cur_word = b
+            cur_cnt = 1
+    return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res)
+
+def solution(text):
+    return min(compress(text, tok_len) for tok_len in list(range(1, int(len(text)/2) + 1)) + [len(text)])
