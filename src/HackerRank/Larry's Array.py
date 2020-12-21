e1d0e9d99ac587388d8103f1beb9cf71e51396a1
Park Junhwa <52024566+melodist@users.noreply.github.com>
2020-12-21 13:21:39 +0900

    Create Larry's Array.py

---
+++ src/HackerRank/Larry's Array.py
@@ -0,0 +1,161 @@
+"""
+https://www.hackerrank.com/challenges/larrys-array/problem
+Rotation don't change the parity of inversions
+ABC (0) -> BCA (+2) -> CAB (0) -> ABC (-2)
+"""
+#1. Solution using comparing each pair (n^2)
+def larrysArray(A):
+    n = len(A)
+    invs = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if A[j] < A[i]:
+                invs += 1
+                
+    return 'YES' if invs % 2 == 0 else 'NO'
+    
+#2. Solution using self-balancing binary search tree (nlogn)
+# An AVL Tree based Python program to  
+# count inversion in an array  
+  
+# A utility function to get height of  
+# the tree rooted with N  
+def height(N): 
+    if N == None:  
+        return 0
+    return N.height 
+  
+# A utility function to size of the 
+# tree of rooted with N  
+def size(N): 
+    if N == None:  
+        return 0
+    return N.size 
+  
+# Helper function that allocates a new  
+# Node with the given key and NULL left 
+# and right pointers.  
+class newNode: 
+    def __init__(self, key): 
+        self.key = key  
+        self.left = self.right = None
+        self.height = self.size = 1
+  
+# A utility function to right rotate 
+# subtree rooted with y  
+def rightRotate(y): 
+    x = y.left  
+    T2 = x.right  
+  
+    # Perform rotation  
+    x.right = y  
+    y.left = T2  
+  
+    # Update heights  
+    y.height = max(height(y.left),  
+                   height(y.right)) + 1
+    x.height = max(height(x.left),  
+                   height(x.right)) + 1
+  
+    # Update sizes  
+    y.size = size(y.left) + size(y.right) + 1
+    x.size = size(x.left) + size(x.right) + 1
+  
+    # Return new root  
+    return x 
+  
+# A utility function to left rotate  
+# subtree rooted with x  
+def leftRotate(x): 
+    y = x.right  
+    T2 = y.left  
+  
+    # Perform rotation  
+    y.left = x  
+    x.right = T2  
+  
+    # Update heights  
+    x.height = max(height(x.left),  
+                   height(x.right)) + 1
+    y.height = max(height(y.left),  
+                   height(y.right)) + 1
+  
+    # Update sizes  
+    x.size = size(x.left) + size(x.right) + 1
+    y.size = size(y.left) + size(y.right) + 1
+  
+    # Return new root  
+    return y 
+  
+# Get Balance factor of Node N  
+def getBalance(N): 
+    if N == None: 
+        return 0
+    return height(N.left) - height(N.right) 
+  
+# Inserts a new key to the tree rotted  
+# with Node. Also, updates *result (inversion count)  
+def insert(node, key, result): 
+      
+    # 1. Perform the normal BST rotation  
+    if node == None:  
+        return newNode(key) 
+  
+    if key < node.key: 
+        node.left = insert(node.left, key, result)  
+  
+        # UPDATE COUNT OF GREATE ELEMENTS FOR KEY  
+        result[0] = result[0] + size(node.right) + 1
+    else: 
+        node.right = insert(node.right, key, result)  
+  
+    # 2. Update height and size of this ancestor node  
+    node.height = max(height(node.left),     
+                      height(node.right)) + 1
+    node.size = size(node.left) + size(node.right) + 1
+  
+    # 3. Get the balance factor of this ancestor 
+    #     node to check whether this node became  
+    #    unbalanced  
+    balance = getBalance(node)  
+  
+    # If this node becomes unbalanced,   
+    # then there are 4 cases  
+  
+    # Left Left Case  
+    if (balance > 1 and key < node.left.key):  
+        return rightRotate(node)  
+  
+    # Right Right Case  
+    if (balance < -1 and key > node.right.key): 
+        return leftRotate(node)  
+  
+    # Left Right Case  
+    if balance > 1 and key > node.left.key: 
+        node.left = leftRotate(node.left)  
+        return rightRotate(node) 
+  
+    # Right Left Case  
+    if balance < -1 and key < node.right.key: 
+        node.right = rightRotate(node.right)  
+        return leftRotate(node) 
+  
+    # return the (unchanged) node pointer  
+    return node 
+  
+
+def larrysArray(A):
+    root = None # Create empty AVL Tree  
+  
+    result = [0] # Initialize result  
+  
+    # Starting from first element, insert all  
+    # elements one by one in an AVL tree.  
+    for i in range(n):  
+        # Note that address of result is passed  
+        # as insert operation updates result by  
+        # adding count of elements greater than  
+        # arr[i] on left of arr[i]  
+        root = insert(root, A[i], result)  
+  
+    return 'YES' if result[0] % 2 == 0 else 'NO'
