365a4b4031156180b212b2c37d59f3a3a8a95946
Park Junhwa <52024566+melodist@users.noreply.github.com>
2021-06-12 11:52:11 +0900

    Update 2064_IP Address.py

--- src/Baekjoon/2064_IP Address.py
+++ src/Baekjoon/2064_IP Address.py
@@ -36,3 +36,33 @@ mask = bin(2**32 - 2**(32-m))[2:]
 mask_concat = '0' * (32 - len(mask)) + mask
 masks = [int(mask_concat[i*8:(i+1)*8], 2) for i in range(4)]
 print('.'.join(map(str, masks)))
+
+#2. Other Solution (64ms)
+import sys
+def solution():
+    T = int(sys.stdin.readline())
+    ips = [sys.stdin.readline().rstrip().split('.') for i in range(T)]
+    net_addr = []
+    net_mask = []
+    for i in range(4):
+        min_ip = int(ips[0][i])
+        max_ip = int(ips[0][i])
+        for tmp_ip in ips:
+            if max_ip < int(tmp_ip[i]):
+                max_ip = int(tmp_ip[i])
+            if min_ip > int(tmp_ip[i]):
+                min_ip = int(tmp_ip[i])
+        if 255 == 256 + (~max_ip^min_ip):
+            net_mask.append(255)
+        else:
+            for j in range(9):
+                if -(~max_ip^min_ip) <= 1<<j:
+                    net_mask.append(256 - (1<<j))
+                    for k in range(3):
+                        net_mask.append(0)
+                    break
+        net_addr.append(int(ips[0][i])&net_mask[i])
+    print("{}.{}.{}.{}".format(net_addr[0], net_addr[1], net_addr[2], net_addr[3]))
+    print("{}.{}.{}.{}".format(net_mask[0], net_mask[1], net_mask[2], net_mask[3]))
+
+solution()
