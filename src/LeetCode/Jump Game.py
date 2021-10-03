81aa79f889e32a1e898a7d3a6d347173e8f54d02
Park Junhwa <52024566+melodist@users.noreply.github.com>
2021-10-03 09:37:47 +0900

    Create Jump Game.py

---
+++ src/LeetCode/Jump Game.py
@@ -0,0 +1,14 @@
+"""
+https://leetcode.com/problems/jump-game/submissions/
+Implementation Problem
+"""
+#1. My Solution
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        cur_right = nums[0]
+        for i in range(len(nums)):
+            if cur_right < i:
+                return False
+            else:
+                cur_right = max(cur_right, nums[i]+i)
+        return True
