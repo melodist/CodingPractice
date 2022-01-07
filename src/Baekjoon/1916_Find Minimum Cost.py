6360916d4e86afe8ab6ecb31bed12d1994ed3731
Park Junhwa <52024566+melodist@users.noreply.github.com>
2022-01-07 13:13:02 +0900

    Update 1916_Find Minimum Cost.py

--- src/Baekjoon/1916_Find Minimum Cost.py
+++ src/Baekjoon/1916_Find Minimum Cost.py
@@ -2,7 +2,7 @@
 https://www.acmicpc.net/problem/1916
 Using Dijkstra
 """
-#1. My Solution
+#1. My Solution (392ms)
 # defaultdict raises memory overflow because of hash table
 import sys
 import heapq
@@ -24,6 +24,8 @@ heapq.heappush(hq, (0, start))
 
 while hq:
     dist, cur = heapq.heappop(hq)
+    if d[cur] < dist:
+        continue
     for b, w in graph[cur]:
         new_d = dist + w
         if new_d < d[b]:
@@ -31,3 +33,46 @@ while hq:
             heapq.heappush(hq, (new_d, b))
     
 print(d[end])
+
+#2. Other Solution (212ms)
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
+
+
+def solve():
+    n = int(input())
+    m = int(input())
+
+    adj = [dict() for _ in range(n+1)]
+
+    for _ in range(m):
+        s, e, c = map(int, input().split())
+        if e in adj[s]:
+            adj[s][e] = min(adj[s][e], c)
+        else:
+            adj[s][e] = c
+
+    start, end = map(int, input().split())
+
+    def dijkstra():
+        dist = [sys.maxsize for _ in range(n+1)]
+        dist[start] = 0
+        hq = [(0, start)]
+        while hq:
+            cost, now = heappop(hq)
+            if dist[now] < cost:
+                continue
+            if now==end:
+                print(cost)
+                exit()
+            for nxt, ncost in adj[now].items():
+                ncost += cost
+                if dist[nxt] > ncost:
+                    dist[nxt] = ncost
+                    heappush(hq, (ncost, nxt))
+    dijkstra()
+
+
+if __name__ == '__main__':
+    solve()
