d25237264a3862f1a6947e899951d0319ecef5c1
Park Junhwa <52024566+melodist@users.noreply.github.com>
2023-01-29 13:59:55 +0900

    Update Super Egg Drop.py

--- src/LeetCode/Super Egg Drop.py
+++ src/LeetCode/Super Egg Drop.py
@@ -44,3 +44,17 @@ class Solution:
             return res
         
         return dp(k, n) # means minimum moves need to determine f with k eggs and n floors
+    
+#2. Other Solution (38ms)
+class Solution:
+    def superEggDrop(self, k: int, n: int) -> int:
+        # dp[k][m] means the highest floor found by at nth floor with k eggs
+        dp = [[0] * (n+1) for _ in range(k+1)]
+
+        m = 0
+        while dp[k][m] < n:
+            m += 1
+            for i in range(1, k+1):
+                dp[i][m] = dp[i][m-1] + dp[i-1][m-1] + 1
+
+        return m
