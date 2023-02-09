694572ed38572951af4f5308fed1dbd52801b6b9
Park Junhwa <52024566+melodist@users.noreply.github.com>
2023-02-09 20:10:59 +0900

    Create Insert into a Binary Search Tree.py

---
+++ src/LeetCode/Insert into a Binary Search Tree.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/insert-into-a-binary-search-tree
+Using Recursion
+"""
+#1. My Solution (140ms)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None: return TreeNode(val)
+        if root.val == val: return root
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+
+        return root
