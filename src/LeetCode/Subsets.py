4562220b819fb1d3bef7d1c99b20e777fbc48875
Park Junhwa <52024566+melodist@users.noreply.github.com>
2023-02-18 18:04:15 +0900

    Update Subsets.py

--- src/LeetCode/Subsets.py
+++ src/LeetCode/Subsets.py
@@ -12,3 +12,18 @@ class Solution:
             res.append(res[i] + [n])
 
         return res
+
+#2. Solution using backtracking (32ms)
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(nums):
+            if not nums: return [[]]
+            n = nums.pop()
+            
+            res = backtrack(nums)
+            for i in range(len(res)):
+                res.append(res[i] + [n])
+
+            return res
+
+        return backtrack(nums)
