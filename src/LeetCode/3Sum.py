9931c254c0ca94eac83bc4ab5aeb56dc44cb74a0
Park Junhwa <52024566+melodist@users.noreply.github.com>
2023-02-25 23:07:30 +0900

    Update 3Sum.py

--- src/LeetCode/3Sum.py
+++ src/LeetCode/3Sum.py
@@ -1,7 +1,8 @@
 """
 https://leetcode.com/problems/3sum/
-Two-Pointer Approach
+Using Two-Pointer Approach
 """
+#1. My Solution (1028ms)
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
@@ -23,3 +24,38 @@ class Solution:
                     j += 1
                     
         return result
+
+#2. Other Solution (1201ms)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        def twoSumTarget(nums, start, target):
+            lo = start
+            hi = len(nums) - 1
+            res = []
+            while lo < hi:
+                left = nums[lo]
+                right = nums[hi]
+                sum_ = left + right
+                if sum_ < target: lo += 1
+                elif sum_ > target: hi -= 1
+                else:
+                    res.append([left, right])
+                    while lo < hi and nums[lo] == left: lo += 1
+                    while lo < hi and nums[hi] == right: hi -= 1
+
+            return res
+
+        nums.sort()
+        n = len(nums)
+        res = []
+        
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            subsets = twoSumTarget(nums, i+1, -nums[i])
+            for s in subsets:
+                res.append(s + [nums[i]])
+
+
+        return res
