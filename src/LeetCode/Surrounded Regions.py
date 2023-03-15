75705d9e329e9c3d1c78c4b146bf535b553901c0
Park Junhwa <52024566+melodist@users.noreply.github.com>
2023-03-15 21:41:20 +0900

    Update Surrounded Regions.py

--- src/LeetCode/Surrounded Regions.py
+++ src/LeetCode/Surrounded Regions.py
@@ -78,3 +78,41 @@ class UnionFind:
 
     def count(self):
         return self.count
+    
+#2. Solution using DFS (153ms)
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def dfs(board, i, j):
+            m = len(board)
+            n = len(board[0])
+            if i < 0 or i >= m or j < 0 or j >= n: return
+
+            if board[i][j] != 'O': return
+
+            board[i][j] = '#'
+            [dfs(board, i+di, j+dj) for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]]
+
+        if len(board) == 0: return
+        m = len(board)
+        n = len(board[0])
+
+        for i in range(m):
+            dfs(board, i, 0)
+            dfs(board, i, n-1)
+
+        for j in range(n):
+            dfs(board, 0, j)
+            dfs(board, m-1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == '#':
+                    board[i][j] = 'O'
